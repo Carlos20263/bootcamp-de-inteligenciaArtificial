08a2e6d7b0ae3c017e329d66bf4c0e0c9b98c1a1
carlosRosero <carlos0345@hotmail.com>
2024-09-06 20:49:12 -0500

    abordamos tipos de datos

---
+++ tipos-de-datos.py
@@ -0,0 +1,12 @@
+"string" # string se utiliza para cadenas de texto
+'sring'# asi tambien podemos generar un string
+"""tus datos son
+nombre:Carlos
+apellidos: Rosero"""#asi generamos un bloque de texto
+# en python tenemos diferentes tipos de numeros
+4#numero entero
+40.2#numero tipo float
+#tambien tenemos el tipo de datos boolean
+True#verdadero
+False#falso
+
