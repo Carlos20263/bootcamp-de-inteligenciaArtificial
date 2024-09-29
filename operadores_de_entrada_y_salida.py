16884c1bc2c0e6b05dcbc74d08118d439481900f
carlosRosero <carlos0345@hotmail.com>
2024-09-28 21:42:59 -0500

    abordamos operaciones de entrada y salida

---
+++ operadores_de_entrada_y_salida.py
@@ -0,0 +1,6 @@
+nombre = input ("¿cuál es tu nombre:")
+print(nombre)
+print(type(nombre))
+edad=input ("¿cuál es tu edad:")
+print (edad)
+print (type(edad)) #el resultado de input siempre será string 
